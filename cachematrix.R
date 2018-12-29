eee4fa2e5f212f851074f6f07397ec7e07a67323
Bin Guo <gb2312@hotmail.com>
2018-12-28 20:33:35 -0500

    Working cached matrix
    tested with
    a <- makeCacheMatrix(diag(2, nrow=2, ncol=2))
    x <- cacheSolve(a)
    x
    x <- cacheSolve(a)
    x

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Programming Assignment 2
+## cache inverse of a matrix
 
-## Write a short comment describing this function
+## Create a special version of matrix with attributes for caching
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) i <<- inverse
+	getinverse <- function() i
+	list(set = set, get = get,
+	     setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Solve the inverse after checking cache first
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	i <- x$getinverse()
+	if (!is.null(i)) {
+		message("getting cached data")
+		return(i)
+	}
+	data <- x$get()
+	i <- solve(data, ...)
+	x$setinverse(i)
+	i
 }
